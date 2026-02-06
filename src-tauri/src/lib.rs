e077e52293b4fbae64bf3935fb5e85117f5419c5
4BMtolobPL <108853516+4BMtolobPL@users.noreply.github.com>
2026-02-06 16:12:43 +0900

    Feature app menu

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,5 @@
 use crate::irc_client::IrcClient;
+use tauri::menu::*;
 use tauri::{Emitter, Manager};
 
 mod irc_client;
@@ -7,25 +8,46 @@ mod irc_client;
 pub fn run() {
     tauri::Builder::default()
         .plugin(
+            // Logging
             tauri_plugin_log::Builder::new()
                 .level(tauri_plugin_log::log::LevelFilter::Trace)
                 .filter(|metadata| metadata.target().starts_with("irc_client_lib"))
                 .build(),
         )
         .setup(|app| {
+            let app_handle = app.handle();
+
+            // Menu
+            let file_menu = SubmenuBuilder::new(app, "File")
+                // .submenu_icon(menu_image) // Optional: Add an icon to the submenu
+                .text("open", "Open")
+                .text("quit", "Quit")
+                .build()?;
+
+            let menu = MenuBuilder::new(app_handle).items(&[&file_menu]).build()?;
+
+            app.set_menu(menu)?;
+
+            /*// Update individual menu item text
+            menu.get("status")
+                .unwrap()
+                .as_menuitem_unchecked()
+                .set_text("Status: Ready")?;*/
+
+            // IRC
             let (tx, rx) = tokio::sync::mpsc::channel(100);
             app.manage(IrcClientState { irc_tx: tx.clone() });
 
-            let app_handle = app.handle().clone();
+            let app_handle_irc = app.handle().clone();
 
             tauri::async_runtime::spawn(async move {
                 match IrcClient::new().await {
                     Ok(client) => {
-                        let _ = irc_client::run_irc_task(client, rx, app_handle).await;
+                        let _ = irc_client::run_irc_task(client, rx, app_handle_irc).await;
                     }
                     Err(e) => {
                         eprintln!("Failed to connect to IRC client: {}", e);
-                        let _ = app_handle.emit("irc:init_error", e.to_string());
+                        let _ = app_handle_irc.emit("irc:init_error", e.to_string());
                     }
                 }
             });

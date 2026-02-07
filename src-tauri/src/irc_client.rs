e8c40242b2ebb76dfb11451941f509656db03d95
4BMtolobPL <108853516+4BMtolobPL@users.noreply.github.com>
2026-02-07 22:47:22 +0900

    Feature: irc receive

---
+++ src-tauri/src/irc_client.rs
@@ -0,0 +1,71 @@
+use crate::IrcClientState;
+use futures::StreamExt;
+use irc::client::prelude::*;
+use tauri::Emitter;
+
+pub(crate) enum IrcCommand {
+    Send { channel: String, message: String },
+    JoinChannel(String),
+    Quit,
+}
+
+pub(crate) struct IrcClient {
+    client: Client,
+}
+
+impl IrcClient {
+    pub(crate) async fn new(/*handle: tauri::AppHandle*/) -> Result<Self, anyhow::Error> {
+        // let config_dir = handle.path().app_config_dir(); TODO: 설정 폴더 이동
+        let config = Config::load("config.toml")?;
+        let client = Client::from_config(config).await?;
+        client.identify()?;
+
+        Ok(Self { client })
+    }
+}
+
+/// IRC Client는 이 함수만 소유
+pub(crate) async fn run_irc_task(
+    mut irc_client: IrcClient,
+    mut rx: tokio::sync::mpsc::Receiver<IrcCommand>,
+    app_handle: tauri::AppHandle,
+) -> Result<(), anyhow::Error> {
+    let mut stream = irc_client.client.stream()?;
+
+    while let Some(message) = stream.next().await.transpose()? {
+        match message.command {
+            Command::PRIVMSG(msg_target, msg) => {
+                let _ = app_handle.emit("irc:message", (msg_target, msg));
+            }
+            _ => {}
+        }
+    }
+
+    Ok(())
+}
+
+#[tauri::command]
+pub(crate) async fn send_message(
+    state: tauri::State<'_, IrcClientState>,
+    channel: String,
+    message: String,
+) -> Result<(), String> {
+    state
+        .irc_tx
+        .send(IrcCommand::Send { channel, message })
+        .await
+        .map_err(|e| e.to_string())
+    // TODO: 자체 에러 타입 구현
+    /*
+    #[derive(Debug, Serialize)]
+    #[serde(tag = "type", content = "data")]
+    enum IrcErrorDto {
+        NotConnected,
+        AuthFailed,
+        IoError(String),
+        Unknown(String),
+    }
+    이런느낌으루다가.
+    아니면 thiserror 같이 써도 될듯.
+    */
+}
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,37 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use crate::irc_client::IrcClient;
+use tauri::{Emitter, Manager};
+
+mod irc_client;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .setup(|app| {
+            let (tx, rx) = tokio::sync::mpsc::channel(100);
+            app.manage(IrcClientState { irc_tx: tx.clone() });
+
+            let app_handle = app.handle().clone();
+
+            tauri::async_runtime::spawn(async move {
+                match IrcClient::new().await {
+                    Ok(client) => {
+                        let _ = irc_client::run_irc_task(client, rx, app_handle).await;
+                    }
+                    Err(e) => {
+                        eprintln!("Failed to connect to IRC client: {}", e);
+                        let _ = app_handle.emit("irc:init_error", e.to_string());
+                    }
+                }
+            });
+
+            Ok(())
+        })
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![irc_client::send_message])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+struct IrcClientState {
+    irc_tx: tokio::sync::mpsc::Sender<irc_client::IrcCommand>,
 }
